915a3be04973dec1a57d31ed072b05da89af28ca
aabhisheksinngh <aabhisheksinngh@gmail.com>
2021-12-10 17:41:17 +0000

    New Script

--- myscript.sh
+++ myscript.sh
@@ -2,13 +2,13 @@
 
 declare -i number1
 declare -i number2
-declare -i multiple
+declare -i add
 
 echo "Enter number1"
 	read number1
 echo "Enter Number2"
 	read number2
-multiple=$((number1 * number2))
-echo Multiple is $multiple
+add=$((number1 + number2))
+echo Multiple is $add
 exit 0
 
